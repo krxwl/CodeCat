008dfcdd77084e8f931921924943e93ac9c26236
ivanova_veronika <nike23450@mail.ru>
2023-08-17 12:29:59 +0300

    added new snackbars with errors and warnings when logging in, added log in with firebase

--- app/src/main/java/com/github/krxwl/codecat/LoginActivity.kt
+++ app/src/main/java/com/github/krxwl/codecat/LoginActivity.kt
@@ -6,15 +6,21 @@ import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.github.krxwl.codecat.databinding.FragmentAuthenticationBinding
 import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 private const val TAG = "LoginActivity"
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: FragmentAuthenticationBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Login)
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
 
 
         binding = FragmentAuthenticationBinding.inflate(layoutInflater)
@@ -22,26 +28,35 @@ class LoginActivity : AppCompatActivity() {
         //setContentView(R.layout.fragment_authentication)
 
         binding.loginButton.setOnClickListener {
-            if (binding.emailTextInput.editText?.text?.isBlank() == true ||
-                binding.emailTextInput.editText?.text?.isEmpty() == true) {
+            val email = binding.emailTextInput.editText?.text.toString()
+            val password = binding.passwordTextInput.editText?.text.toString()
 
-                val snackbar = Snackbar.make(binding.fragmentAuth, R.string.enter_an_email, Snackbar.LENGTH_SHORT)
-                snackbar.view.layoutParams = (snackbar.view.layoutParams as FrameLayout.LayoutParams).apply {
+            val snackbar = Snackbar.make(binding.fragmentAuth, "", Snackbar.LENGTH_SHORT)
+            snackbar.view.layoutParams =
+                (snackbar.view.layoutParams as FrameLayout.LayoutParams).apply {
                     gravity = Gravity.TOP
                 }
+            if (email.isBlank() || email.isEmpty()) {
+                snackbar.setText(R.string.enter_an_email)
                 snackbar.show()
-
+            } else if (!email.contains('@')) {
+                snackbar.setText(R.string.enter_the_correct_email)
+                snackbar.show()
+            } else if (password.isBlank() || password.isEmpty()) {
+                snackbar.setText(R.string.enter_your_password)
+                snackbar.show()
+            } else {
+                auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                    if (it.isSuccessful) {
+                        snackbar.setText("Successfully LoggedIn")
+                    } else {
+                        snackbar.setText("Log In failed")
+                    }
+                    snackbar.show()
+                }
             }
         }
-
-        fun Snackbar.gravityTop() {
-            this.view.layoutParams = (this.view.layoutParams as FrameLayout.LayoutParams).apply {
-                gravity = Gravity.TOP
-            }
-        }
-
     }
-
 
 
 }
